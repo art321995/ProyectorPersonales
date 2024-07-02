93b0c3a27d6944fc4c4cbe5451bb65732ee7b403
Arturo Esqueda <art270112@gmail.com>
2024-07-02 11:23:48 -0600

    Formulario funcionando al 100

--- inovission/src/components/SaberMas.jsx
+++ inovission/src/components/SaberMas.jsx
@@ -1,34 +1,22 @@
-import {useRef} from "react";
+import { useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import emailjs from "@emailjs/browser";
-
 
 
 const validationSchema = Yup.object().shape({
     nombre: Yup.string().min(2,"El nombre es muy corto").max(50,"El nombre es muy largo").required("Campo Obligatorio"),
     apellido: Yup.string().min(3,"El nombre es muy corto").max(50,"El nombre es muy largo").required("Campo Obligatorio"),
     email: Yup.string().email("Email invalido").required("Compo Obligatorio"),
-    telefono: Yup.number().min(8,"Telefono incorrecto").max(14, "Telefono incorrecto").required("Compo Obligatorio"),
+    telefono: Yup.number().min(8,"Telefono incorrecto, minimo 8 digitos").required("Compo Obligatorio"),
     comentario: Yup.string().min(5,"Mensaje corto").max(1000,"Llegaste al maximo permitido").required("Compo Obligatorio")
 })
 
 
 const SaberMas = () => {
 
-  const refForm = useRef ();
-
-  const handleSubmit = (event) =>{
-    event.preventDefault();
-    
-    const serviceId = "service_hvbshpf";
-    const templateId = "template_gifccpl";
-    const apikey= "VA5dpLKYARhtWPSe0";
-
-    emailjs.sendForm(serviceId, templateId, refForm.current, apikey)
-    .then((result) => console.log(result.text))
-    .catch((err) => console.error(err));
-   }
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const formik = useFormik({
     initialValues: {
@@ -38,17 +26,41 @@ const SaberMas = () => {
       telefono: "",
       comentario: ""
     },
-    validationSchema,
-    onSubmit: values =>{
-      console.log(values)
-    }
+    validationSchema: validationSchema,
+    onSubmit: (values, { resetForm }) => {
+      sendEmail(values);
+      resetForm();
+    },
   })
+
+  const sendEmail = (formData) => {
+    const serviceId = "service_hvbshpf"; 
+    const templateId = "template_gifccpl"; 
+    const userId = "VA5dpLKYARhtWPSe0"; 
+
+    emailjs.send(serviceId, templateId, formData, userId)
+      .then((response) => {
+        console.log('Email enviado correctamente!', response.status, response.text);
+        setSuccessMessage('¡Email enviado correctamente, Personal de Optica Inno Vision te contactará!');
+        setErrorMessage(null); 
+      })
+      .catch((error) => {
+        console.error('Error al enviar el email:', error);
+        setErrorMessage('Hubo un error al enviar el email. Por favor, inténtalo de nuevo más tarde.');
+        setSuccessMessage(null);
+      });
+  };
+
+  
   return (
     <div className="contacto">
       <h1>Saber Mas Sobre Nosotros</h1>
       <h3>Eres una empresa y te gustaria que hicieramos examenes gratis en tus instalaciones, envianos tu informacion y nos pondremos en contacto contigo!</h3>
+        
+        {successMessage && <p className="success-message">{successMessage}</p>}
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
 
-      <form ref={refForm} onSubmit={handleSubmit}  action="" className="formulario">
+      <form  onSubmit={formik.handleSubmit}  action="" className="formulario">
         <div className="form-group">
           <label htmlFor="nombre">Nombre(s)</label>
           <input type="text" id="nombre" name="nombre" value={formik.values.nombre} onChange={formik.handleChange} />
@@ -76,7 +88,7 @@ const SaberMas = () => {
         </div>
 
         <button type="submit" className="btnEnviarF" value="Enviar" >Enviar</button>
-
+        
       </form>
     </div>
   )
