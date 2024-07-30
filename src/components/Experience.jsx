20ac5262dcccca11325880ddb1aa3351b06731f8
Arturo Esqueda <art270112@gmail.com>
2024-07-30 16:36:45 -0600

    Se corrige titulo de Experiencia

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,7 +8,7 @@ const Experience = () => {
           whileInView={{opacity: 1, y: 0}}
           initial={{ opacity: 0, y: -100}}
           transition={{ duration: 1.5 }}  
-          className="my-20 text-center text-4xl">Experience
+          className="my-20 text-center text-4xl">Experiencia
         </motion.h2>
         <div>
             {EXPERIENCES.map((experience, index) =>(
