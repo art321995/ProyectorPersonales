b790a541a35e77a9728cddd907fc5d2aa8746b8a
Arturo Esqueda <art270112@gmail.com>
2024-09-23 17:16:02 -0600

    Se corrigen las imagenes del modulo de Pinatas

--- src/components/Pinatas.jsx
+++ src/components/Pinatas.jsx
@@ -34,19 +34,18 @@ const Pinatas = () => {
               </p>
           </motion.div>
             <div className="grid grid-cols-2 gap-3 p-4 md:grid-cols-5" >
-                <a href="#"><img src="./img/Pinata1.jpg" alt="Pinata1" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
-                <a href="#"><img src="./img/Pinata2.jpg" alt="Pinata2" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
-                <a href="#"><img src="./img/Pinata3.jpg" alt="Pinata3" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
-                <a href="#"><img src="./img/Pinata4.jpg" alt="Pinata4" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
-                <a href="#"><img src="./img/Pinata5.jpg" alt="Pinata5" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
-                <a href="#"><img src="./img/Pinata6.jpg" alt="Pinata6" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
-                <a href="#"><img src="./img/Pinata7.jpg" alt="Pinata7" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
-                <a href="#"><img src="./img/Pinata8.jpg" alt="Pinata8" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
-                <a href="#"><img src="./img/Pinata9.jpg" alt="Pinata9" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
-                <a href="#"><img src="./img/Pinata10.jpg" alt="Pinata10" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
+                <a onClick={enviarMensajeWhatsApp}><img src="./img/Pinata1.jpg" alt="Pinata1" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
+                <a onClick={enviarMensajeWhatsApp}><img src="./img/Pinata2.jpg" alt="Pinata2" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
+                <a onClick={enviarMensajeWhatsApp}><img src="./img/Pinata3.jpg" alt="Pinata3" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
+                <a onClick={enviarMensajeWhatsApp}><img src="./img/Pinata4.jpg" alt="Pinata4" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
+                <a onClick={enviarMensajeWhatsApp}><img src="./img/Pinata5.jpg" alt="Pinata5" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
+                <a onClick={enviarMensajeWhatsApp}><img src="./img/Pinata6.jpg" alt="Pinata6" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
+                <a onClick={enviarMensajeWhatsApp}><img src="./img/Pinata7.jpg" alt="Pinata7" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
+                <a onClick={enviarMensajeWhatsApp}><img src="./img/Pinata8.jpg" alt="Pinata8" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
+                <a onClick={enviarMensajeWhatsApp}><img src="./img/Pinata9.jpg" alt="Pinata9" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
+                <a onClick={enviarMensajeWhatsApp}><img src="./img/Pinata10.jpg" alt="Pinata10" className="rounded-3xl opacity-70 hover:opacity-100 transition duration-300" ></img></a>
             </div>
         </div>
-        
     </section>
   )
 }
