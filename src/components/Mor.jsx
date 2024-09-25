ccd1b7021f88e85c18bf9b1c72e06beeed9eed07
Arturo Esqueda <art270112@gmail.com>
2024-09-25 17:08:59 -0600

    Se ajusta el texto en Mas.. y al Nabvar se hace la opcion que cuando se elija un menu en panatallas chicas el menu se quita

--- src/components/Mor.jsx
+++ src/components/Mor.jsx
@@ -20,22 +20,22 @@ const Mor = () => {
             className="grid grid-cols-2 lg:gap-16 gap-2 p-4 md:grid-cols-4">
                 <div className="text-center items-center">
                     <img className="rounded-2xl" src="./img/Botana.jpg"></img>
-                    <h4 className="lg:text-2xl text-xl font-bold lg:py-4 py-2 text-yellow-600">- Botana -</h4>
-                    <p>Churros - Papas - Cacahuates - Semillas - Gomitas</p>
+                    <h4 className="lg:text-2xl text-lg font-bold lg:py-4 py-2 text-yellow-600">- Botana -</h4>
+                    <p className="lg:text-base text-sm">Churros - Papas - Cacahuates - Semillas - Gomitas</p>
                 </div><div className="text-center items-center">
                     <img className="rounded-2xl" src="./img/Desechables.jpg"></img>
-                    <h4 className="lg:text-2xl text-xl font-bold lg:py-4 py-2 text-yellow-600">- Desechables -</h4>
-                    <p>Platos - Vasos - Servilletas - Bolsas - Cubiertos - Charolas</p>
+                    <h4 className="lg:text-2xl text-lg font-bold lg:py-4 py-2 text-yellow-600">- Desechables -</h4>
+                    <p className="lg:text-base text-sm">Platos - Vasos - Servilletas - Bolsas - Cubiertos - Charolas</p>
                 </div>
                 <div className="text-center items-center">
                     <img className="rounded-2xl" src="./img/Velas.jpg"></img>
-                    <h4 className="lg:text-2xl text-xl font-bold lg:py-4 py-2 text-yellow-600">- Velas -</h4>
-                    <p>Velas de chispas - Feliz Cumpleaños - Mágicas - Números</p>
+                    <h4 className="lg:text-2xl text-lg font-bold lg:py-4 py-2 text-yellow-600">- Velas -</h4>
+                    <p className="lg:text-base text-sm">Velas de chispas - Feliz Cumpleaños - Mágicas - Números</p>
                 </div>
                 <div className="text-center items-center">
                     <img className="rounded-2xl" src="./img/Letreros.jpg"></img>
-                    <h4 className="lg:text-2xl text-xl font-bold lg:py-4 py-2 text-yellow-600">- Letreros -</h4>
-                    <p>Banderin - Banner Metalico y Mate - De Cortina - Led - Letras en Globo</p>
+                    <h4 className="lg:text-2xl text-lg font-bold lg:py-4 py-2 text-yellow-600">- Letreros -</h4>
+                    <p className="lg:text-base text-sm">Banderin - Banner Metalico y Mate - De Cortina - Led - Letras en Globo</p>
                 </div>
         </motion.div>
         <motion.div 
@@ -45,22 +45,22 @@ const Mor = () => {
             className="grid grid-cols-2 lg:gap-16 gap-2 p-4 md:grid-cols-4">
             <div className="text-center items-center">
                 <img className="rounded-2xl" src="./img/Globos.jpg"></img>
-                <h4 className="lg:text-2xl text-xl font-bold lg:py-4 py-2 text-yellow-600">- Globos -</h4>
-                <p>Látex - Metálicos - Foil - De Número - Con Temática</p>
+                <h4 className="lg:text-2xl text-lg font-bold lg:py-4 py-2 text-yellow-600">- Globos -</h4>
+                <p className="lg:text-base text-sm">Látex - Metálicos - Foil - De Número - Con Temática</p>
             </div><div className="text-center items-center">
                 <img className="rounded-2xl" src="./img/Regalos.jpg"></img>
-                <h4 className="lg:text-2xl text-xl font-bold lg:py-4 py-2 text-yellow-600">- Regalos -</h4>
-                <p>Juguetes - Termos - Plumones - Balones - Audífonos - Carteras</p>
+                <h4 className="lg:text-2xl text-lg font-bold lg:py-4 py-2 text-yellow-600">- Regalos -</h4>
+                <p className="lg:text-base text-sm">Juguetes - Termos - Plumones - Balones - Audífonos - Carteras</p>
             </div>
             <div className="text-center items-center">
                 <img className="rounded-2xl" src="./img/Reposteria.jpg"></img>
-                <h4 className="lg:text-2xl text-xl font-bold lg:py-4 py-2 text-yellow-600">- Reposteria -</h4>
-                <p>Harinas - Crema Batida - Coberturas - Concentrados - Colorantes - Capacillos</p>
+                <h4 className="lg:text-2xl text-lg font-bold lg:py-4 py-2 text-yellow-600">- Reposteria -</h4>
+                <p className="lg:text-base text-sm">Harinas - Crema Batida - Coberturas - Concentrados - Colorantes - Capacillos</p>
             </div>
             <div className="text-center items-center">
                 <img className="rounded-2xl" src="./img/Galletas.jpg"></img>
-                <h4 className="lg:text-2xl text-xl font-bold lg:py-4 py-2 text-yellow-600">- Galletas -</h4>
-                <p>Surtido - Birotito - Polvorón - Orejas</p>
+                <h4 className="lg:text-2xl text-lg font-bold lg:py-4 py-2 text-yellow-600">- Galletas -</h4>
+                <p className="lg:text-base text-sm">Surtido - Birotito - Polvorón - Orejas</p>
             </div>
         </motion.div>
     </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const enviarMensajeWhatsApp = () => {
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  
+
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
@@ -27,63 +27,63 @@ const Navbar = () => {
         top: offsetPosition,
         behavior: "smooth"
       });
+      setIsMobileMenuOpen(false);
     }
   };
 
   return (
     <div>
       <nav className="fixed top-4 z-50 flex w-full flex-col items-center justify-center">
-      <div className="flex w-full items-center justify-between overflow-y-hidden p-4 backdrop-blur-lg lg:m-2 lg:w-[50rem]  lg:shadow-lg">
-        <div className="hidden space-x-6 lg:flex"> 
-          <a href="#" className="hover:opacity-50 hover:underline cursor-pointer">Inicio</a> 
+        <div className="flex w-full items-center justify-between overflow-y-hidden p-4 backdrop-blur-lg lg:m-2 lg:w-[50rem]  lg:shadow-lg">
+          <div className="hidden space-x-6 lg:flex"> 
+            <a href="#" className="hover:opacity-50 hover:underline cursor-pointer">Inicio</a> 
+          </div>
+          <div className="hidden space-x-6 lg:flex"> 
+            <a onClick={() => handleScroll("about")} className="hover:opacity-50 hover:underline cursor-pointer">Acerca De</a> 
+          </div>
+          <div className="hidden space-x-6 lg:flex"> 
+            <a onClick={() => handleScroll("brands")} className="hover:opacity-50 hover:underline cursor-pointer">Marcas</a> 
+          </div>
+          <div className="hidden space-x-6 lg:flex"> 
+            <a onClick={() => handleScroll("pinatas")} className="hover:opacity-50 hover:underline cursor-pointer">Piñatas</a> 
+          </div>
+          <div className="hidden space-x-6 lg:flex"> 
+            <a onClick={() => handleScroll("mor")} className="hover:opacity-50 hover:underline cursor-pointer">Más...</a> 
+          </div>
+          <div className="hidden space-x-6 lg:flex"> 
+            <a onClick={() => handleScroll("footer")} className="hover:opacity-50 hover:underline cursor-pointer">Contacto</a> 
+          </div>
+          <div className="hidden space-x-6 lg:flex"> 
+            <a onClick={enviarMensajeWhatsApp} className="hover:opacity-50 hover:underline text-yellow-600 font-bold cursor-pointer">Haz tu Pedido!</a> 
+          </div>
+          <div className="flex lg:hidden">
+            <button onClick={toggleMobileMenu} className="flex items-center">
+              {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+            </button>
+            <p className="flex pl-3 text-yellow-600 font-bold">Dulcería Imperial</p>
+          </div>
         </div>
-        <div className="hidden space-x-6 lg:flex"> 
-          <a onClick={() => handleScroll("about")} className="hover:opacity-50 hover:underline cursor-pointer">Acerca De</a> 
-        </div>
-        <div className="hidden space-x-6 lg:flex"> 
-          <a onClick={() => handleScroll("brands")} className="hover:opacity-50 hover:underline cursor-pointer">Marcas</a> 
-        </div>
-        <div className="hidden space-x-6 lg:flex"> 
-          <a onClick={() => handleScroll("pinatas")} className="hover:opacity-50 hover:underline cursor-pointer">Piñatas</a> 
-        </div>
-        <div className="hidden space-x-6 lg:flex"> 
-          <a onClick={() => handleScroll("mor")} className="hover:opacity-50 hover:underline cursor-pointer">Más...</a> 
-        </div>
-        <div className="hidden space-x-6 lg:flex"> 
-          <a onClick={() => handleScroll("footer")} className="hover:opacity-50 hover:underline cursor-pointer">Contacto</a> 
-        </div>
-        <div className="hidden space-x-6 lg:flex"> 
-          <a onClick={enviarMensajeWhatsApp} className="hover:opacity-50 hover:underline text-yellow-600 font-bold cursor-pointer">Haz tu Pedido!</a> 
-        </div>
-        <div className="flex lg:hidden">
-          <button onClick={toggleMobileMenu} className="flex items-center">
-            {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
-          </button>
-          <p className="flex pl-3 text-yellow-600 font-bold">Dulcería Imperial</p>
-        </div>
-      </div>
-      {isMobileMenuOpen && (
-        <div className="w-full backdrop-blur-lg lg:hidden">
-          <a href="#" className="block p-3 tracking-tighter cursor-pointer">Inicio</a>
-          <a onClick={() => handleScroll("about")} className="block p-3 tracking-tighter cursor-pointer">Acerca De</a>
-          <a onClick={() => handleScroll("brands")} className="block p-3 tracking-tighter cursor-pointer">Marcas</a>
-          <a onClick={() => handleScroll("pinatas")} className="block p-3 tracking-tighter cursor-pointer">Piñatas</a>
-          <a onClick={() => handleScroll("mor")} className="block p-3 tracking-tighter cursor-pointer">Más...</a>
-          <a onClick={() => handleScroll("footer")} className="block p-3 tracking-tighter cursor-pointer">Contacto</a>
-          <a onClick={enviarMensajeWhatsApp} className="block p-3 tracking-tighter text-yellow-600 font-bold cursor-pointer">Haz tu Pedido!</a>
-        </div>
+        {isMobileMenuOpen && (
+          <div className="w-full backdrop-blur-lg lg:hidden">
+            <a href="#" className="block p-3 tracking-tighter cursor-pointer" onClick={toggleMobileMenu}>Inicio</a>
+            <a onClick={() => handleScroll("about")} className="block p-3 tracking-tighter cursor-pointer">Acerca De</a>
+            <a onClick={() => handleScroll("brands")} className="block p-3 tracking-tighter cursor-pointer">Marcas</a>
+            <a onClick={() => handleScroll("pinatas")} className="block p-3 tracking-tighter cursor-pointer">Piñatas</a>
+            <a onClick={() => handleScroll("mor")} className="block p-3 tracking-tighter cursor-pointer">Más...</a>
+            <a onClick={() => handleScroll("footer")} className="block p-3 tracking-tighter cursor-pointer">Contacto</a>
+            <a onClick={enviarMensajeWhatsApp} className="block p-3 tracking-tighter text-yellow-600 font-bold cursor-pointer">Haz tu Pedido!</a>
+          </div>
         )}
       </nav>
       <motion.div 
-        initial={{opacity: 0, y: -50}}
-        whileInView={{opacity: 1, y: 0}}
-        viewport={{once: true}}
-        transition={{ duration: 1.5, delay: 0.2}}
-        className="flex flex-wrap lg:w-[500px] lg:h-[500px] mx-auto my-20 ">
-          <img src="./img/Portada.png" alt="Portada" className="w-full rounded-3xl p-4"></img>
+        initial={{ opacity: 0, y: -50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1.5, delay: 0.2 }}
+        className="flex flex-wrap lg:w-[500px] lg:h-[500px] mx-auto my-20">
+        <img src="./img/Portada.png" alt="Portada" className="w-full rounded-3xl p-4" />
       </motion.div>
     </div>
-    
   );
 };
 
