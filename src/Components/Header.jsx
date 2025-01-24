8d0579e1431b587be9d2c12084ce1b9d358c17d6
Arturo Esqueda <art270112@gmail.com>
2025-01-24 16:25:39 -0600

    Se agrega el Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -35,11 +35,15 @@ const Header = () => {
         }
     ]
   return (
-    <div>
-        <img src={logo} className='w-[80px] md:w-[115px] object-cover'/>
-        {menu.map((item) =>
-            <Headeritem/>
-         )}
+    <div className='flex items-center justify-between p-5'>
+        <div className='flex gap-8 items-center'>
+            <img src={logo} className='w-[80px] md:w-[115px] object-cover'/>
+            {menu.map((item) =>
+                <Headeritem name={item.name} Icon={item.icon}/>
+            )}
+        </div>
+        <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+            className='w-[40px] rounded-full'/>
     </div>
   )
 }
--- src/Components/Headeritem.jsx
+++ src/Components/Headeritem.jsx
@@ -1,8 +1,12 @@
 
 
-const Headeritem = () => {
+const Headeritem = ({name, Icon}) => {
   return (
-    <div>Headeritem</div>
+    <div className="text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer 
+    hover:underline underline-offset-8">
+      <Icon/>
+      <h2>{name}</h2>
+    </div>
   )
 }
 
