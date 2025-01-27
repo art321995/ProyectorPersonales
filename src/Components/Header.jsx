33679fbb03c65509c65bfe05e53675b8c45502d1
Arturo Esqueda <art270112@gmail.com>
2025-01-27 16:49:09 -0600

    Se realiza responsive el Nabvar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,15 @@
 import logo from './../assets/Images/logo.png';
-import { HiHome } from 'react-icons/hi';
+import { HiDotsVertical, HiHome } from 'react-icons/hi';
 import { HiMagnifyingGlass } from 'react-icons/hi2';
 import { HiStar } from 'react-icons/hi';
 import { HiPlayCircle } from 'react-icons/hi2';
 import { HiTv } from 'react-icons/hi2';
 import { HiPlus } from 'react-icons/hi';
 import Headeritem from './Headeritem';
+import { useState } from 'react';
 
 const Header = () => {
+    const [toggle, setToggle] = useState(false);
     const menu =[
         {
             name:'HOME',
@@ -38,9 +40,24 @@ const Header = () => {
     <div className='flex items-center justify-between p-5'>
         <div className='flex gap-8 items-center'>
             <img src={logo} className='w-[80px] md:w-[115px] object-cover'/>
-            {menu.map((item) =>
-                <Headeritem name={item.name} Icon={item.icon}/>
-            )}
+            <div className='hidden md:flex gap-8'>
+                {menu.map((item) =>
+                    <Headeritem name={item.name} Icon={item.icon}/>
+                )}
+            </div>
+            <div className='flex md:hidden gap-5'>
+                {menu.map((item, index) => index <3&&(
+                    <Headeritem name={''} Icon={item.icon}/>
+                ))}
+                <div className='md:hidden' onClick={()=>setToggle(!toggle)}>
+                    <Headeritem name={''} Icon={HiDotsVertical} />
+                    {toggle ? <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
+                        {menu.map((item, index) => index>2&&(
+                        <Headeritem  name={item.name} Icon={item.icon}/>
+                ))}
+                    </div>:null}
+                </div>
+            </div>
         </div>
         <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
             className='w-[40px] rounded-full'/>
--- src/Components/Headeritem.jsx
+++ src/Components/Headeritem.jsx
@@ -3,9 +3,9 @@
 const Headeritem = ({name, Icon}) => {
   return (
     <div className="text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer 
-    hover:underline underline-offset-8">
+    hover:underline underline-offset-8 mb-2">
       <Icon/>
-      <h2>{name}</h2>
+      <h2 className="">{name}</h2>
     </div>
   )
 }
