31f357e388ea42ee808af2e686034186877d2f75
Arturo Esqueda <art270112@gmail.com>
2025-01-29 17:16:47 -0600

    Se realiza el slider con estilos

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -3,6 +3,15 @@ import axios from "axios";
 const movieBaseUrl="https://api.themoviedb.org/3"
 const api_key = '462f939716f3b191525e6138c960b105'
 
-const getTrendingVideos = axios.get(this.movieBaseUrl + "/trending/all/day?api_key" + api_key);
+const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=462f939716f3b191525e6138c960b105';
 
-export default{ getTrendingVideos}
+
+const getTrendingVideos = axios.get(movieBaseUrl+"/trending/all/day?api_key="+api_key);
+
+const getMovieByGenreId=(id)=>
+    axios.get(movieByGenreBaseURL+"&with_genres="+id)
+
+export default{
+    getTrendingVideos,
+    getMovieByGenreId
+};
