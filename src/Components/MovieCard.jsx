bf862a934a2bffae8012075e0947a5c8d29957d0
Arturo Esqueda <art270112@gmail.com>
2025-02-10 14:00:07 -0600

    Se termina la pagina del clone de Disney Plus

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -11,4 +11,4 @@ function MovieCard({movie}) {
   )
 }
 
-export default MovieCard
+export default MovieCard;
--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -14,7 +14,6 @@ function MovieList({genreId,index_}) {
 
     const getMovieByGenreId=()=>{
         GlobalApi.getMovieByGenreId(genreId).then(resp=>{
-            // console.log(resp.data.results)
             setMovieList(resp.data.results)
         })
     }
@@ -28,26 +27,22 @@ function MovieList({genreId,index_}) {
   return (
     <div className='relative'>
          <IoChevronBackOutline onClick={()=>slideLeft(elementRef.current)} 
-         className={`text-[50px] text-white
-           p-2 z-10 cursor-pointer 
-            hidden md:block absolute
+         className={`text-[50px] text-white p-2 z-10 cursor-pointer hidden md:block absolute 
             ${index_%3==0?'mt-[80px]':'mt-[150px]'} `}/>
    
-    <div ref={elementRef} className='flex overflow-x-auto gap-8
-     scrollbar-none scroll-smooth pt-4 px-3 pb-4'>
-        {movieList.map((item,index)=>(
-           <>
-          {index_%3==0?<HrMovieCard movie={item}/>:<MovieCard movie={item} />}
-           </> 
-        ))}
-    </div>
-    <IoChevronForwardOutline onClick={()=>slideRight(elementRef.current)}
-           className={`text-[50px] text-white hidden md:block
-           p-2 cursor-pointer z-10 top-0
-            absolute right-0 
-            ${index_%3==0?'mt-[80px]':'mt-[150px]'}`}/> 
+        <div ref={elementRef} className='flex overflow-x-auto gap-8
+        scrollbar-none scroll-smooth pt-4 px-3 pb-4' style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }} >
+            {movieList.map((item)=>(
+            <>
+            {index_%3==0?<HrMovieCard movie={item}/>:<MovieCard movie={item} />}
+            </> 
+            ))}
+        </div>
+        <IoChevronForwardOutline onClick={()=>slideRight(elementRef.current)}
+            className={`text-[50px] text-white hidden md:block p-2 cursor-pointer z-10 top-0 absolute right-0 
+                ${index_%3==0?'mt-[80px]':'mt-[150px]'}`}/> 
     </div>
   )
 }
 
-export default MovieList
+export default MovieList;
